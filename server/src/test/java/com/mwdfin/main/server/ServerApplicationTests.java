ac3e4885ed9904cfe863dd4ebf2bea1ba632b4c5
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-10-15 18:29:01 +0100

    first commit

---
+++ server/src/test/java/com/mwdfin/main/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwdfin.main.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
